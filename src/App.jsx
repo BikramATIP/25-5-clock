1418c4681f47077d553c53f2b61c902dec6750a1
Bikram Thapa <bikramt755@gmail>
2025-01-23 11:16:22 +1100

    finish styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faSync } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
@@ -7,35 +9,34 @@ function App() {
   return (
     <>
     <div className="clock">
-      <h1 className="title">Title</h1>
+      <h1 className="title">25 + 5 Clock</h1>
       <div className="top-controls-container">
-       <div className="break-length-container">
+       <div className="break-length-container top-control">
         <span id="break-label" className="break-label">Break Length</span>
         <div className="break-controls">
           <button id="break-decrement" className="break-decrement">-</button>
-          <span className="break-length" id="break-length">5</span>
+          <span className="break-length length-time" id="break-length">5</span>
           <button id="break-increment" className="break-increment">+</button>
         </div>
        </div>
-       <div className="session-length-container">
-        <span id="session-label" className="session-label">Session Length</span>
+       <div className="session-length-container top-control">
         <div className="session-controls">
          <span id="session-label" className="session-label">Session Length</span>
          <div className="session-controls">
           <button id="session-decrement" className="session-decrement">-</button>
-          <span id="session-length" className="session-length">25</span>
+          <span id="session-length" className="session-length length-time">25</span>
           <button id="session-increment" className="session-increment">+</button>
          </div>
         </div>
        </div>
       </div>   {/* end of top-controls-container */}
       <div className="timer-container">
-       <div id="timer-label" classname="timer-label">Session</div>
+       <div id="timer-label" className="timer-label">Session</div>
        <div id="time-left" className="time-left">25:00</div>
       </div>
       <div className='bottom-controls-container'>
-       <button id="start_stop" className='start_stop'>Start/Stop</button>
-       <button id="reset" className='reset'>Reset</button>
+       <button id="start_stop" className='start_stop'><FontAwesomeIcon icon={faPlay} /><FontAwesomeIcon icon={faPause} /></button>
+       <button id="reset" className='reset'><FontAwesomeIcon icon={faSync} /></button>
         </div>
     </div>
     </>
